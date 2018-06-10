8e9fb1bfe7794bfb478b71bf801abdf0811fe133
huyongbing <hyb_1989@126.com>
2018-06-10 10:37:08 +0800

    按照题意要求还原ListNode

--- src/main/java/leetcode/test02/ListNode.java
+++ src/main/java/leetcode/test02/ListNode.java
@@ -19,20 +19,4 @@ public class ListNode {
 		val = x ;
 	}
 
-	public int getVal() {
-		return val;
-	}
-
-	public void setVal(int val) {
-		this.val = val;
-	}
-
-	public ListNode getNext() {
-		return next;
-	}
-
-	public void setNext(ListNode next) {
-		this.next = next;
-	}
-	
 }
