6d45f6bb18fd5a31b3a35b7f4b8f2ba791aaa47a
huyongbing <hyb_1989@126.com>
2018-06-10 13:54:31 +0800

    leetcode---两数相加

--- src/main/java/leetcode/test02/Solution.java
+++ src/main/java/leetcode/test02/Solution.java
@@ -16,68 +16,136 @@ public class Solution {
 		ListNode node1 = new ListNode(2);
 		ListNode node2 = new ListNode(4);
 		ListNode node3 = new ListNode(3);
-		node1.setNext(node2);
-		node2.setNext(node3);
+		node1.next = node2;
+		node2.next = node3;
 
 		ListNode node4 = new ListNode(5);
 		ListNode node5 = new ListNode(6);
 		ListNode node6 = new ListNode(4);
-		node4.setNext(node5);
-		node5.setNext(node6);
-
-		addTwoNumbers(node1, node4);
-		// System.out.println(getNodeNumber(node1));
-		// System.out.println(getNodeNumber(node4));
+		node4.next = node5;
+		node5.next = node6;
+		
+		ListNode resultNode = addTwoNumbers(node1, node4) ;
+		System.out.println(getNodeNumber(resultNode));
 
 	}
 
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+	/**
+	 * @param node1
+	 * @param node4
+	 * @return
+	 */
+	private static ListNode addTwoNumbers(ListNode node1, ListNode node4) {
+		
+		return getResultNode(node1, node4);
+		
+	}
+
+
+	/**
+	 * @param node1
+	 * @param node4
+	 * @return
+	 */
+	private static ListNode getResultNode(ListNode node1, ListNode node4) {
+		
 		ListNode resultNode = null;
-
-		int first = getNodeNumber(l1);
-		int second = getNodeNumber(l2);
-
-		int result = first + second;
-
-		resultNode = transIntToNode(result);
-
-		return resultNode;
-
-	}
-
-	/**
-	 * @param result
-	 * @return
-	 */
-	private static ListNode transIntToNode(int result) {
-
-		String str = String.valueOf(result);
-		StringBuilder sb = new StringBuilder(str);
-		char[] charArr = sb.reverse().toString().toCharArray();
-		ListNode l = null;
-		for (char oneChar : charArr) {
-			int num = oneChar - '0';
-			if (l == null) {
-				l = new ListNode(num);
+		// 商 --> 进位
+		int quotient = 0 ;
+		// 余数
+		int remainder = 0 ;
+		while(! breakCondition(node1, node4)) {
+			
+			int sum = calcRootNumSum(node1, node4) + quotient;
+			quotient = sum / 10 ;
+			remainder = sum % 10 ;
+			
+			if(null == resultNode) {
+				resultNode = new ListNode(remainder);
 			} else {
-				if (null == l.getNext()) {
-					l.setNext(new ListNode(num));
-				} else {
-					
-				}
+				setLastNodeVal(resultNode, remainder);
 			}
+			
+			if(null != node1) {
+				node1 = node1.next;
+			}
+			
+			if(null != node4) {
+				node4 = node4.next;
+			}
+			
 		}
-
-		return l;
+		
+		if(0 != quotient) {
+			setLastNodeVal(resultNode, quotient) ;
+		}
+		
+		return resultNode;
 	}
 
+
 	/**
-	 * @param l1
+	 * @param resultNode
+	 * @param val
+	 */
+	private static void setLastNodeVal(ListNode resultNode, int val) {
+		ListNode tempRoot = resultNode;
+		while(null != tempRoot.next){
+			tempRoot = tempRoot.next;
+		}
+		tempRoot.next = new ListNode(val);
+	}
+
+
+	/**
+	 * @param node1
+	 * @param node4
 	 * @return
 	 */
-	private static int getNodeNumber(ListNode l1) {
-		if (l1 == null || (null != l1 && l1.val == 0)) {
+	private static int calcRootNumSum(ListNode node1, ListNode node4) {
+		
+		int val1 = getRootValue(node1);
+		int val2 = getRootValue(node4);
+		
+		return val1 + val2;
+		
+	}
+
+
+	/**
+	 * @param node1
+	 * @param node4
+	 * @param resultNode 
+	 * @return
+	 */
+	private static boolean breakCondition(ListNode node1, ListNode node4) {
+		
+		if(null == node1 && null == node4) {
+			return true ;
+		}
+		
+		return false ;
+	}
+
+
+	/**
+	 * @param node1
+	 * @return
+	 */
+	private static int getRootValue(ListNode node1) {
+		
+		if(null == node1) {
+			return 0;
+		}
+		
+		int val = node1.val;
+		node1 = node1.next;
+		return val;
+	}
+	
+	private static long getNodeNumber(ListNode l1) {
+		if (l1 == null) {
 			return 0;
 		}
 
@@ -89,7 +157,7 @@ public class Solution {
 			start = start.next;
 		}
 
-		return Integer.valueOf(sb.reverse().toString());
+		return Long.valueOf(sb.reverse().toString());
 
 	}
 
